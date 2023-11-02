ac0fb273d5a4140438abe317e221c2aaf022a523
s-rruia <s-rruia@486E294123.lwsd.k12wa.org>
2023-11-02 15:15:53 -0700

    not done

--- src/Mirror.java
+++ src/Mirror.java
@@ -2,8 +2,15 @@
 public class Mirror {
 
 	public static void main(String[] args) {
-
-
+	System.out.println(line());
+	
+	System.out.println(line());
+	}
+	public static String line() {
+		String top = "";
+		for(int i = 1; i <= 16; i++)
+		top += "=";
+		return "#" + top + "#" ;
 	}
 
 }
