2dae07d19a66cccdc581d83962e75bb8d27fcb4d
s-rruia <s-rruia@486E294123.lwsd.k12wa.org>
2023-11-07 14:54:11 -0800

    done

--- src/Mirror.java
+++ src/Mirror.java
@@ -1,25 +1,57 @@
 
 public class Mirror {
-
+	private static final int size = 3;
 	public static void main(String[] args) {
 		//print top line
 	line();
 	// middle top
 	topHalf();
 	//middle bottom
+	bottomHalf();
 	line();
 	//bottom
 	}
 	
 	public static void line() {
 		String top = "";
-		for(int i = 1; i <= 16; i++)
+		for(int i = 1; i <= size * 4; i++)
 			top += "=";
 		System.out.println("#" + top + "#");
 	}
 
 	public static void topHalf() {
-		System.out.print("|");
+		for(int line = 1; line <= size; line++) {
+			System.out.print("|");
+			for(int space = 1; space <= line * -2 + 2 * size; space++) {
+				System.out.print(" ");
+			}
+			System.out.print("<>");
+			for(int dots = 1; dots <= line *4 - 4; dots++ ) {
+				System.out.print(".");
+			}
+			System.out.print("<>");
+			for(int space = 1; space <= line * -2 + 2 * size; space++) {
+				System.out.print(" ");
+			}
+			System.out.println("|");
+		}
 		
 	}
+	public static void bottomHalf() {
+		for(int line = size; line >= 1; line--) {
+				System.out.print("|");
+				for(int space = 1; space <= line * -2 + 2 * size; space++) {
+					System.out.print(" ");
+				}
+				System.out.print("<>");
+				for(int dots = 1; dots <= line *4 - 4; dots++ ) {
+					System.out.print(".");
+				}
+				System.out.print("<>");
+				for(int space = 1; space <= line * -2 + 2 * size; space++) {
+					System.out.print(" ");
+				}
+				System.out.println("|");
+			}
+		}
 }
