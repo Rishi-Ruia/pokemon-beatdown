bc1af7aeec033c3d5b6f0a78cfa228883632eae5
s-rruia <s-rruia@486M168356.lwsd.k12wa.org>
2024-03-18 12:35:30 -0700

    data is weird

--- src/Game.java
+++ src/Game.java
@@ -5,5 +5,16 @@ public class Game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GUI game = new GUI();
+		Scanner scan = new Scanner(System.in);
+		 //List<String> CNames = Arrays.asList("Copy of All Pokemon Spreadsheet");
+	}
+	public static void attack(Pokemon attacker, String attack, Pokemon attacked) {
+		double stab =1;
+		if(attacker.getType1().equals(attack) || attacker.getType2().equals(attack)) stab = 1.5;
+		double random = stab *(Math.random()*0.15 +0.85) ; 
+		int damage = (int) (((((((2*100)/5)+2)*attacker.getAttack()
+					*Integer.MIN_VALUE)/attacked.getDefense())/50)+2);
+		damage = (int) Math.floor(damage *random);
+		attacked.setHp(damage);
 	}
 }
