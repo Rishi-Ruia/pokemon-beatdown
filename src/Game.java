ac54016823b952647b496d0feeb8fd09c63ab33e
s-rruia <s-rruia@486M168356.lwsd.k12wa.org>
2024-03-18 13:05:29 -0700

    data is weird

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Game {
 
-	public static void main(String[] args) {
+public class Game {
+	
+	private Pokemon[] poke = new Pokemon[800];
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		GUI game = new GUI();
 		Scanner scan = new Scanner(System.in);
 		 //List<String> CNames = Arrays.asList("Copy of All Pokemon Spreadsheet");
+		String line = "";
+		String[][] pokemons = new String[801][13];
+		BufferedReader br = new BufferedReader(new FileReader("Copy of All Pokemon Data Spreadsheet_exported.csv"));
+		int i = 0;
+		while((line = br.readLine()) != null) {
+			String[] pokemon = line.split(",");
+			pokemons[i] = pokemon;
+			i++;
+		}
+		for(i = 0; i < pokemons.length; i++) {
+			poke[i] = new Pokemon(pokemons[i][])
+		}
+		
 	}
 	public static void attack(Pokemon attacker, String attack, Pokemon attacked) {
 		double stab =1;
