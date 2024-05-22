f7034f8348217972f7990cb94c8ddb35dc4c9226
Raeed <s-rrahman@lwsd.org>
2024-05-21 19:15:37 -0700

    Made GUI Changes

--- src/GUI.java
+++ src/GUI.java
@@ -57,11 +57,11 @@ public class GUI extends JFrame implements ActionListener{
 		AIname.setFont(new Font("Arial", Font.BOLD, 80));
 		AIname.setBounds(1000, -250 , 1000, 1000);
 		this.add(AIname);
-		console.setBounds(20, 0, 2000, 2000);		
-		console.setFont(new Font("Arial", Font.ITALIC, 50));
+		console.setBounds(width / 6, (int) (height - height / 4), width - width / 6, height / 12);		
+		console.setFont(new Font("Arial", Font.ITALIC, 30));
 		this.add(console);
-		AIconsole.setBounds(20, 200, 2000, 2000);		
-		AIconsole.setFont(new Font("Arial", Font.ITALIC, 50));
+		AIconsole.setBounds(width / 6, (int) (height - height / 5), width - width / 6, height / 12);		
+		AIconsole.setFont(new Font("Arial", Font.ITALIC, 30));
 		AIconsole.setAlignmentX(JFrame.CENTER_ALIGNMENT);
 		console.setAlignmentX(JFrame.CENTER_ALIGNMENT);
 		this.add(AIconsole);
@@ -70,22 +70,22 @@ public class GUI extends JFrame implements ActionListener{
 	//adds the moves and resizes them to the Jframe
 	public void addMoves() {
 		move1 = new JButton(user.getCurrent().getMove(0).getName());
-		move1.setBounds(width / 6, (int) (height - height / 2.5), (int) (width / 6.5), height / 14);
+		move1.setBounds(width / 6, (int) (height - height / 3), (int) (width / 6.5), height / 14);
 		move1.setFont(new Font("Arial", Font.PLAIN, 20));
 		move1.setBackground(Color.WHITE);
 		move1.addActionListener(this);
 		move2 = new JButton(user.getCurrent().getMove(1).getName());
-		move2.setBounds(2 * (width / 6), (int) (height - height / 2.5), (int) (width / 6.5), height / 14);
+		move2.setBounds(2 * (width / 6), (int) (height - height / 3), (int) (width / 6.5), height / 14);
 		move2.setFont(new Font("Arial", Font.PLAIN, 20));
 		move2.setBackground(Color.WHITE);
 		move2.addActionListener(this);
 		move3 = new JButton(user.getCurrent().getMove(2).getName());
-		move3.setBounds(3 * (width / 6), (int) (height - height / 2.5), (int) (width / 6.5), height / 14);
+		move3.setBounds(3 * (width / 6), (int) (height - height / 3), (int) (width / 6.5), height / 14);
 		move3.setFont(new Font("Arial", Font.PLAIN, 20));
 		move3.setBackground(Color.WHITE);
 		move3.addActionListener(this);
 		move4 = new JButton(user.getCurrent().getMove(3).getName());
-		move4.setBounds(4 * (width / 6), (int) (height - height / 2.5), (int) (width / 6.5), height / 14);
+		move4.setBounds(4 * (width / 6), (int) (height - height / 3), (int) (width / 6.5), height / 14);
 		move4.setFont(new Font("Arial", Font.PLAIN, 20));
 		move4.setBackground(Color.WHITE);
 		move4.addActionListener(this);
@@ -99,32 +99,32 @@ public class GUI extends JFrame implements ActionListener{
 	private void addSwitch() {
 		pokemon1 = new JButton(user.getPokemon(0).getName());
 		pokemon1.setFont(new Font("Arial", Font.BOLD, 20));
-		pokemon1.setBounds(width / 36, height / 36, 170, 150);
+		pokemon1.setBounds(width / 84, height / 36, width / 8, width / 16);
 		pokemon1.addActionListener(this);
 		this.add(pokemon1);
 		pokemon2 = new JButton(user.getPokemon(1).getName());
 		pokemon2.setFont(new Font("Arial", Font.BOLD, 20));
-		pokemon2.setBounds(1170, 650, 170, 150);
+		pokemon2.setBounds(width / 84, height / 36 + (width / 14), width / 8, width / 16);
 		pokemon2.addActionListener(this);
 		this.add(pokemon2);
 		pokemon3 = new JButton(user.getPokemon(2).getName());
 		pokemon3.setFont(new Font("Arial", Font.BOLD, 20));
-		pokemon3.setBounds(1340, 650, 185, 150);
+		pokemon3.setBounds(width / 84, height / 36 + 2 * (width / 14), width / 8, width / 16);
 		pokemon3.addActionListener(this);
 		this.add(pokemon3);
 		pokemon4 = new JButton(user.getPokemon(3).getName());
 		pokemon4.setFont(new Font("Arial", Font.BOLD, 20));
-		pokemon4.setBounds(1000, 800, 170, 150);
+		pokemon4.setBounds(width / 84, height / 36 + 3 * (width / 14), width / 8, width / 16);
 		pokemon4.addActionListener(this);
 		this.add(pokemon4);
 		pokemon5 = new JButton(user.getPokemon(4).getName());
 		pokemon5.setFont(new Font("Arial", Font.BOLD, 20));
-		pokemon5.setBounds(1170, 800, 170, 150);
+		pokemon5.setBounds(width / 84, height / 36 + 4 * (width / 14), width / 8, width / 16);
 		pokemon5.addActionListener(this);
 		this.add(pokemon5);
 		pokemon6 = new JButton(user.getPokemon(5).getName());
 		pokemon6.setFont(new Font("Arial", Font.BOLD, 20));
-		pokemon6.setBounds(1340, 800, 185, 150);
+		pokemon6.setBounds(width / 84, height / 36 + 5 * (width / 14), width / 8, width / 16);
 		pokemon6.addActionListener(this);
 		this.add(pokemon6);
 		this.setVisible(true);
@@ -243,4 +243,13 @@ public class GUI extends JFrame implements ActionListener{
 		move4.setEnabled(true);
 		this.setVisible(true);
 	}
+	
+//	public void userHP() {
+//		JProgressBar hp = new JProgressBar(0, user.getCurrent().getHp());	
+//		hp.setBounds();
+//	}
+//	
+//	public void aiHP() {
+//		
+//	}
 }
